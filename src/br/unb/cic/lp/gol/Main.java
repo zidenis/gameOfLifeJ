49031e1add06a3644e67eed9e5ffd4fd52f4e5fe
Denis Albuquerque <denisalbuquerque@gmail.com>
2014-11-30 10:23:50 -0200

    simplifying main

--- src/br/unb/cic/lp/gol/Main.java
+++ src/br/unb/cic/lp/gol/Main.java
@@ -1,18 +1,13 @@
 package br.unb.cic.lp.gol;
 
 public class Main {
-
-    public static final int GRID_HEIGHT = 20;
-    public static final int GRID_WIDTH = 30;
+    public static final int GRID_WIDTH = 25;
+    public static final int GRID_HEIGHT = 25;
     
     public static void main(String args[]) {
         GameController controller = new GameController();
-
-        Statistics statistics = new Statistics();
-        GameEngine engine = new GameEngine(GRID_HEIGHT, GRID_WIDTH, statistics);
-        GameView board = new GameViewSwing(controller, engine);        
+        controller.initController();
+        GameView board = new GameViewSwing(controller);
         controller.setBoard(board);
-        controller.setEngine(engine);
-        controller.setStatistics(statistics);
     }
 }
