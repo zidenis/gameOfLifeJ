84e375e65b37fbe408f10eababa71656238f822a
Denis Albuquerque <denisalbuquerque@gmail.com>
2014-11-30 14:55:50 -0200

    Memento to preserve game states

---
+++ src/br/unb/cic/lp/gol/Memento.java
@@ -0,0 +1,38 @@
+package br.unb.cic.lp.gol;
+
+/**
+ *
+ * @author zidenis
+ */
+public class Memento {
+    private Cell[][] cells;
+    private Statistics stats;
+    
+    public Memento(Cell[][] cells, Statistics statistics) {
+        this.cells = cells;
+        this.stats = statistics;
+    }
+    
+    public Cell[][] getCells() {
+        return cells;
+    }
+
+    public Statistics getStatistics() {
+        return stats;
+    }
+
+    public void setStatistics(Statistics statistics) {
+        this.stats = statistics;
+    }
+    
+    public Memento duplicate() {
+        Statistics newStatistics = new Statistics(stats.getCreatedCells(), stats.getGeneratedCells(), stats.getKilledCells(), stats.getAlivedCells(), stats.getNumOfGenerations());
+        Memento newState = new Memento(new Cell[Main.GRID_HEIGHT][Main.GRID_WIDTH], newStatistics);
+        for (int i = 0; i < Main.GRID_HEIGHT; i++) {
+            for (int j = 0; j < Main.GRID_WIDTH; j++) {
+                newState.getCells()[i][j] = this.getCells()[i][j].duplicate();
+            }
+        }
+        return newState;
+    }
+}
